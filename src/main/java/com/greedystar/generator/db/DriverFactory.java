6450c5f1718fec724279f80624f9554eb650280e
GreedyStar <greedystar@163.com>
2019-04-04 18:30:16 +0800

    new:Add higher version driver class name of mysql

--- src/main/java/com/greedystar/generator/db/DriverFactory.java
+++ src/main/java/com/greedystar/generator/db/DriverFactory.java
@@ -5,13 +5,18 @@ package com.greedystar.generator.db;
  * Date   2018-10-24
  */
 public class DriverFactory {
-    private final static String DRIVER_MYSQL = "com.mysql.jdbc.Driver";
+    private final static String DRIVER_MYSQL_5 = "com.mysql.jdbc.Driver";
+    private final static String DRIVER_MYSQL_UPER = "com.mysql.cj.jdbc.Driver";
     private final static String DRIVER_ORACLE = "oracle.jdbc.driver.OracleDriver";
     private final static String DRIVER_SQLSERVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
 
     public static String getDriver(String url) {
         if (url.contains("mysql")) {
-            return DRIVER_MYSQL;
+            if (url.contains("serverTimezone")) {
+                return DRIVER_MYSQL_UPER;
+            } else {
+                return DRIVER_MYSQL_5;
+            }
         }
         if (url.contains("oracle")) {
             return DRIVER_ORACLE;
